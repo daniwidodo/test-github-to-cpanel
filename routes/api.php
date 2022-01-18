de62a9f4d29d36bc75e5c665e79e292796f8d956
bangtur_studio <dani.widodo87@gmail.com>
2022-01-18 10:47:27 +0800

    test2

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 Route::get('/test', function ()
 {
-    return 'Hello World';
+    return 'Hello World from my desktop';
 });
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
